a15617dde255451e1259146db220a991b7179cdd
Lijuan Tang <tang.lij@northeastern.edu>
2026-03-30 13:11:09 -0700

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    List<List<Integer>> res;
+    List<Integer> path;
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        res = new ArrayList<>();
+        path = new ArrayList<>();
+        Arrays.sort(nums);
+        dfs(nums, 0);
+        return res;
+    }
+    private void dfs(int[] nums, int start) {
+        res.add(new ArrayList<>(path));
+        if (start == nums.length) {
+            return;
+        }
+        for(int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            path.add(nums[i]);
+            dfs(nums, i + 1);
+            path.remove(path.size() - 1);
+        }
+    }
+}
