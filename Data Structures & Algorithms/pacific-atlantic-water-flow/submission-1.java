8ecf148c07a63b060cbf0d9f0ec97fab6c7ec1ad
Lijuan Tang <tang.lij@northeastern.edu>
2026-04-02 20:33:00 -0700

    Add: pacific-atlantic-water-flow - submission-1

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.java
@@ -0,0 +1,42 @@
+class Solution {
+    boolean[][] pac, atl;
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        pac = new boolean[m][n];
+        atl = new boolean[m][n];
+        List<List<Integer>> res = new ArrayList<>();
+        
+        for (int col = 0; col < n; col++) {
+            dfs(heights, 0, col, heights[0][col], pac);
+            dfs(heights, m - 1, col, heights[m - 1][col], atl);
+        }
+        for (int row = 0; row < m; row++) {
+            dfs(heights, row, 0, heights[row][0], pac);
+            dfs(heights, row, n - 1, heights[row][n - 1], atl);
+        }
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                List<Integer> cur = Arrays.asList(i, j);
+                if (pac[i][j] && atl[i][j]) {
+                    res.add(cur);
+                }
+            }
+        }
+        return res;
+    }
+    private void dfs(int[][] heights, int row, int col, int preHeight, boolean[][] visited) {
+        int m = heights.length;
+        int n = heights[0].length;
+        if (row < 0 || row >= m || col < 0 || col >= n) return;
+        if (visited[row][col]) return;
+        if (heights[row][col] < preHeight) return;
+        visited[row][col] = true;
+        dfs(heights, row + 1, col, heights[row][col], visited);
+        dfs(heights, row - 1, col, heights[row][col], visited);
+        dfs(heights, row, col - 1, heights[row][col], visited);
+        dfs(heights, row, col + 1, heights[row][col], visited);
+    }
+
+}
