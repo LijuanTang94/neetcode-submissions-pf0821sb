5e5e5d01c73ac90b67dab94a966208179f7df253
Lijuan Tang <tang.lij@northeastern.edu>
2026-04-08 21:13:04 -0700

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        int n = hand.length;
+        if (n % groupSize != 0) return false;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int h: hand) {
+            map.put(h, map.getOrDefault(h, 0) + 1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(Integer key: map.keySet()) {
+            pq.offer(key);
+        }
+        while (!pq.isEmpty()) {
+            int start = pq.peek();
+            for(int i = start; i < start + groupSize; i++) {
+                if (map.getOrDefault(i, 0).equals(0)) return false;
+                map.put(i, map.get(i) - 1);
+                if (map.get(i).equals(0)) {
+                    if (!pq.peek().equals(i)) return false;
+                    pq.poll();
+                }
+            }
+        }
+        return true;
+    }
+}
