e8040e3dcbb89543da74531531987012f1dd036e
Lijuan Tang <tang.lij@northeastern.edu>
2026-04-12 08:40:49 -0700

    Add: count-subsequences - submission-1

---
+++ Data Structures & Algorithms/count-subsequences/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    Map<String, Integer> map;
+    public int numDistinct(String s, String t) {
+        map = new HashMap<>();
+        return dfs(s, t, 0, 0);
+        
+    }
+    private int dfs(String s, String t, int i, int j) {
+        if (j == t.length()) return 1;
+        if (i == s.length()) return 0;
+        String temp = i + "," + j;
+        if (map.containsKey(temp)) {
+            return map.get(temp);
+        }
+        int res = 0;
+        res += dfs(s, t, i + 1, j);
+        if (s.charAt(i) == t.charAt(j)) {
+            res += dfs(s, t, i + 1, j + 1);
+        }
+        map.put(temp, res);
+        return res;
+    }
+}
