89d1ae7eaf1dc2aafccbea9d281b34fbcd24bf82
Lijuan Tang <tang.lij@northeastern.edu>
2026-04-11 20:35:08 -0700

    Add: longest-increasing-path-in-matrix - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    Map<String, Integer> map;
+    public int longestIncreasingPath(int[][] matrix) {
+        map = new HashMap<>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int res = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                res = Math.max(res, dfs(matrix, i, j, -1));
+            }
+        }
+        return res;
+        
+    }
+    private int dfs(int[][] matrix, int row, int col, int prev) {
+        if (row < 0 || col < 0 || row == matrix.length || col == matrix[0].length || matrix[row][col] <= prev) return 0;
+        if (map.containsKey(row + "," + col)) return map.get(row + "," + col);
+        int res = 1;
+        res = Math.max(res, 1 + dfs(matrix, row + 1, col, matrix[row][col]));
+        res = Math.max(res, 1 + dfs(matrix, row - 1, col, matrix[row][col]));
+        res = Math.max(res, 1 + dfs(matrix, row, col + 1, matrix[row][col]));
+        res = Math.max(res, 1 + dfs(matrix, row, col - 1, matrix[row][col]));
+        map.put(row + "," + col, res);
+        return res;
+    }
+}
