18122c4e27f90cc59fe4a792696b2113c31361a4
Lijuan Tang <tang.lij@northeastern.edu>
2026-04-13 14:31:39 -0700

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.java
@@ -0,0 +1,24 @@
+class KthLargest {
+    PriorityQueue<Integer> pq;
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+        pq = new PriorityQueue<>();
+        this.k = k;
+        for(int i = 0; i < nums.length; i++) {
+            pq.offer(nums[i]);
+            while (pq.size() > k) {
+                pq.poll();
+            }
+        }
+    }
+    
+    public int add(int val) {
+        pq.offer(val);
+        if (pq.size() > k) {
+            pq.poll();
+        }
+        return pq.peek();
+        
+    }
+}
