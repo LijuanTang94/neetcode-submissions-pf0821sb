90477514f512b9848b8a275681ca17cd7989caf4
Lijuan Tang <tang.lij@northeastern.edu>
2026-04-16 18:29:09 -0700

    Add: multiply-strings - submission-2

---
+++ Data Structures & Algorithms/multiply-strings/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        int m = num1.length();
+        int n = num2.length();
+        int[] res = new int[m + n];
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                int sum = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i + j;
+                int p2 = i + j + 1;
+                sum += res[p2];
+                res[p2] = sum % 10;
+                res[p1] += sum / 10;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < m + n; i++) {
+            if (sb.length() == 0 && res[i] == 0) continue;
+            sb.append(res[i]);
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
