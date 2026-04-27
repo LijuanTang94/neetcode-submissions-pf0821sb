a9083c3d6c6bff66e4c51ba801a7b30040c9ae8e
Lijuan Tang <tang.lij@northeastern.edu>
2026-04-26 22:13:42 -0700

    Add: sliding-window-maximum - submission-6

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-6.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        Deque<Integer> dq = new ArrayDeque<>();
+        int[] res = new int[n - k + 1];
+        for(int r = 0, l = 0; r < n; r++) {
+            if (!dq.isEmpty() && dq.peekFirst() < l) {
+                dq.pollFirst();
+            }
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[r]) {
+                dq.pollLast();
+            }
+            dq.offerLast(r);
+            if (r >= k - 1) {
+                res[l] = nums[dq.peekFirst()];
+                l++;
+            }
+        }
+        return res;
+    }
+}
