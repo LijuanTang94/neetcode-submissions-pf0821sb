7f9ef73d4028b0a4edc29a24f86dbb7b0885a9c1
Lijuan Tang <tang.lij@northeastern.edu>
2026-04-27 09:19:18 -0700

    Add: trapping-rain-water - submission-5

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-5.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int trap(int[] height) {
+        int lmax = 0;
+        int rmax = 0;
+        int l = 0, r = height.length - 1;
+        int res = 0;
+        while (l < r) {
+            lmax = Math.max(lmax, height[l]);
+            rmax = Math.max(rmax, height[r]);
+            if (height[l] < height[r]) {
+                res += lmax - height[l];
+                l++;
+            } else {
+                res += rmax - height[r];
+                r--;
+            }
+        }
+        return res;
+    }
+}
