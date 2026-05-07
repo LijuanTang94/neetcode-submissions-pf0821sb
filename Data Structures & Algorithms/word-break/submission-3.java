9d42ac9aa5481c855ae28f5e3e25a3c468568993
Lijuan Tang <tang.lij@northeastern.edu>
2026-05-06 17:31:46 -0700

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    Set<String> set;
+    Map<Integer, Boolean> map;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        map = new HashMap<>();
+        set = new HashSet<>(wordDict);
+        return check(s, 0);
+    }
+    private boolean check(String s, int start) {
+        if (start == s.length()) return true;
+        if (map.containsKey(start)) return map.get(start);
+        boolean res = false;
+        for (int j = start + 1; j <= s.length(); j++) {
+            if (set.contains(s.substring(start, j)) && check(s, j)) {
+                res = true;
+                break;
+            }
+        }
+        map.put(start, res);
+        return res;
+    }
+}
