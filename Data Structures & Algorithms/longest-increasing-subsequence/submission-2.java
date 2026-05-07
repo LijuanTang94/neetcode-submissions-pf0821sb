c110ac7e34a5d4abc9de6bce47af6be7495a9a4c
Lijuan Tang <tang.lij@northeastern.edu>
2026-05-06 17:59:45 -0700

    Add: longest-increasing-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    int max = 0;
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return 1;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+
+    }
+}
