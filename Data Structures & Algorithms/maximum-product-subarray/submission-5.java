ca0c34b5c1cde9e6090015b458845b37411117a6
Lijuan Tang <tang.lij@northeastern.edu>
2026-05-06 17:25:15 -0700

    Add: maximum-product-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-5.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int min = nums[0];
+        int max = nums[0];
+        int res = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int temp = min;
+                min = max;
+                max = temp;
+            }
+            min = Math.min(min * nums[i], nums[i]);
+            max = Math.max(max * nums[i], nums[i]);
+            res = Math.max(res, max);
+        }
+        return res;
+    }
+}
