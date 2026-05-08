f93c7f6ff30fd76e02ad072c27e591843ba1f9ba
Lijuan Tang <tang.lij@northeastern.edu>
2026-05-07 21:58:14 -0700

    Add: buy-and-sell-crypto-with-cooldown - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // 0 hold 1 sell 2 cool
+        int n = prices.length;
+        if (n == 1) return 0;
+        int[][] dp = new int[n][3];
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2] - prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1]);
+        }
+        return dp[n - 1][1];
+    }
+}
