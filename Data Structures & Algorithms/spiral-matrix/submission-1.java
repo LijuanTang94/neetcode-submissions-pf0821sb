0149709071f19513c27563327449ea0de866657b
Lijuan Tang <tang.lij@northeastern.edu>
2026-05-09 21:55:40 -0700

    Add: spiral-matrix - submission-1

---
+++ Data Structures & Algorithms/spiral-matrix/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int t = 0, l = 0, b = m - 1, r = n - 1;
+        while (t <= b && l <= r) {
+            for(int j = l; j <= r; j++) {
+                res.add(matrix[t][j]);
+            }
+            t++;
+            for(int i = t; i <= b; i++) {
+                res.add(matrix[i][r]);
+            }
+            r--;
+            if (t <= b) {
+                for(int j = r; j >= l; j--) {
+                    res.add(matrix[b][j]);
+                }
+                b--;
+            }
+            if (l <= r) {
+                for(int i = b; i >= t; i--) {
+                    res.add(matrix[i][l]);
+                }
+                l++;
+            }
+        }
+        return res;
+    }
+}
