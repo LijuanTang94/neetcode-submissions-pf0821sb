d2511a83f9b3896e5f35bf69d0c2982a0768a295
Lijuan Tang <tang.lij@northeastern.edu>
2026-05-11 16:35:10 -0700

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int l = 0;
+        Set<Integer> set = new HashSet<>();
+        for(int r = 0; r < nums.length; r++) {
+            if (set.contains(nums[r])) return true;
+            set.add(nums[r]);
+            if (r - l + 1 > k) {
+                set.remove(nums[l++]);
+            }
+        }
+        return false;
+    }
+}
