2b02bb6d6829711166cdfd1bc2145c35de92af35
Lijuan Tang <tang.lij@northeastern.edu>
2026-05-11 12:52:49 -0700

    Add: valid-sudoku - submission-7

---
+++ Data Structures & Algorithms/valid-sudoku/submission-7.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] rows = new boolean[9][9], cols = new boolean[9][9], boxes = new boolean[9][9];
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                int num = board[i][j] - '1';
+                int idx = i / 3 * 3 + j / 3;
+                if (rows[i][num] || cols[j][num] || boxes[idx][num]) {
+                    return false;
+                }
+                rows[i][num] = true;
+                cols[j][num] = true;
+                boxes[idx][num] = true;
+            }
+        }
+        return true;
+    }
+}
