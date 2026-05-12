65f121f8d05d4c7620c211f01b203ac5d7efe679
Lijuan Tang <tang.lij@northeastern.edu>
2026-05-12 11:48:05 -0700

    Add: search-in-rotated-sorted-array-ii - submission-0

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return true;
+            } 
+            if (nums[l] == nums[r] && nums[l] == nums[m]) {
+                l++;
+                r--;
+                continue;
+            }
+            if (nums[m] >= nums[l]) {
+                if (nums[l] <= target && target < nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                if (nums[m] < target && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
