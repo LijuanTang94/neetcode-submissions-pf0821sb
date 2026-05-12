92632e6d89ec1ff07f4d106f6b918459e6515684
Lijuan Tang <tang.lij@northeastern.edu>
2026-05-12 15:51:18 -0700

    Add: copy-linked-list-with-random-pointer - submission-7

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-7.java
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    Map<Node, Node> map;
+    public Node copyRandomList(Node head) {
+        map = new HashMap<>();
+        return copy(head);
+    }
+    private Node copy(Node head) {
+        if (head == null) return null;
+        if (map.containsKey(head)) {
+            return map.get(head);
+        }
+        Node newNode = new Node(head.val);
+        map.put(head, newNode);
+        newNode.next = copy(head.next);
+        newNode.random = copy(head.random);
+        return newNode;
+    }
+}
